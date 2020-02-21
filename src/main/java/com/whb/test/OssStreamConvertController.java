0456041ea53fd5ec027561bf6296555304899bd8
wanghengbin <hengbinwang@163.com>
2020-02-21 16:27:12 +0800

    test submit

---
+++ src/main/java/com/whb/test/OssStreamConvertController.java
@@ -0,0 +1,259 @@
+package com.whb.test;
+
+import com.unnet.yjs.annotation.HttpMethod;
+import com.unnet.yjs.base.ContainerProperties;
+import com.unnet.yjs.entity.OssFileRecord;
+import com.unnet.yjs.service.OssFileRecordService;
+import com.unnet.yjs.util.IOssOperation;
+import com.xiaoleilu.hutool.util.StrUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.connector.ClientAbortException;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+/**
+ * Email: love1208tt@foxmail.com
+ * Copyright (c)  2019. missbe
+ * @author lyg   19-7-29 下午9:21
+ *
+ *
+ **/
+@RestController
+@Api(tags = "OssStreamConvertController", description = "文件上传控制器")
+@RequestMapping("/api/v1/resource/file/")
+@Slf4j
+public class OssStreamConvertController {
+    @Resource
+    private ContainerProperties containerProperties;
+/*
+    @Autowired
+    @Qualifier("paasOssTool")
+    private IOssOperation paasOssTool;
+    @Autowired
+    @Qualifier("minIoOssTool")
+    private IOssOperation minIoOssTool;
+
+    @Resource
+    private OssFileRecordService ossFileRecordService;*/
+
+
+    /**
+     * 对象存储中转请求链接-根据文件名字请求对象存储的文件流
+     *
+     * @param fileName 文件名称
+     */
+    @GetMapping("video/{fileName}")
+    @ApiOperation(value = "对象存储文件流中转接口", httpMethod = HttpMethod.GET)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "fileName", value = "文件名称", paramType = "path")
+    })
+    public void videoPlayer(@PathVariable(value = "fileName") String fileName, HttpServletRequest request,HttpServletResponse response) throws IOException {
+/*        if(paasOssTool == null || minIoOssTool == null){
+            OutputStream out = response.getOutputStream();
+            out.write(("OSS文件服务器配置出现问题,请修复后重试.").getBytes());
+            out.flush();
+            out.close();
+            return;
+        }*/
+
+        ///是否开启本地缓存视频mp4文件
+        String filePath = containerProperties.getFileCacheLocation() + fileName;
+        File file = new File(filePath);
+        File parentDir = file.getParentFile();
+        if (!parentDir.exists()) {
+            boolean isMakeParentDir = parentDir.mkdirs();
+            if(isMakeParentDir){
+                log.info("创建文件夹{}成功.",parentDir.getAbsolutePath());
+            }else {
+                log.error("创建文件夹{}失败.",parentDir.getAbsolutePath());
+            }
+        }//end if
+/*        if (!file.exists()) {
+            ///本地文件不存在,从OSS下载到本地
+            boolean isMakeNewFile = file.createNewFile();
+            if(isMakeNewFile){
+                log.info("创建文件{}成功.",file.getAbsolutePath());
+            }else {
+                log.error("创建文件{}失败.",file.getAbsolutePath());
+            }
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            InputStream is = null;
+            try {
+                if (StrUtil.equalsIgnoreCase(containerProperties.getFileUploadType(), "myOss")) {
+                    is = minIoOssTool.load(fileName);
+                }
+                if (StrUtil.equalsIgnoreCase(containerProperties.getFileUploadType(), "paas")) {
+                    is = paasOssTool.load(fileName);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                e.printStackTrace();
+                log.error("对象存储加载文件失败,msg:"+e.getLocalizedMessage());
+                OutputStream out = response.getOutputStream();
+                out.write(("对象存储加载文件失败,msg:"+e.getLocalizedMessage()).getBytes());
+                out.flush();
+                out.close();
+                return;
+            }
+            ////判断流不为空
+            Objects.requireNonNull(is);
+
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+            byte[] buffer = new byte[4096];
+            int length;
+            while ((length = is.read(buffer)) != -1) {
+                bos.write(buffer, 0, length);
+            }
+            bos.flush();
+            bos.close();
+        }*////end if
+        log.info("文件：{}，总长度：{}",file.getName(),file.length());
+        ///对文件执行分块
+        fileChunkDownload(filePath,request,response);
+/*        /////添加文件访问记录
+        OssFileRecord ossFileRecord = ossFileRecordService.findByFileName(fileName);
+        if (Objects.nonNull(ossFileRecord)) {
+            ossFileRecord.setFileLength(String.valueOf(file.length()));
+            ossFileRecord.setVisitCount(ossFileRecord.getVisitCount() + 1);
+        }else{
+            ossFileRecord = new OssFileRecord();
+            ossFileRecord.setFileName(fileName);
+            ossFileRecord.setFileLength(String.valueOf(file.length()));
+            ossFileRecord.setVisitCount(1);
+            ossFileRecord.setRemarks("OssFileRecord");
+        }
+        ossFileRecordService.insertOrUpdate(ossFileRecord);*/
+    }
+
+    /**
+     * 文件支持分块下载和断点续传
+     * @param filePath 文件完整路径
+     * @param request 请求
+     * @param response 响应
+     */
+    private void fileChunkDownload(String filePath, HttpServletRequest request,
+                                   HttpServletResponse response) {
+        String range = request.getHeader("Range");
+        log.info("current request rang:" + range);
+        File file = new File(filePath);
+        //开始下载位置
+        long startByte = 0;
+        //结束下载位置
+        long endByte = file.length() - 1;
+        log.info("文件开始位置：{}，文件结束位置：{}，文件总长度：{}", startByte, endByte, file.length());
+
+        //有range的话
+        if (range != null && range.contains("bytes=") && range.contains("-")) {
+            range = range.substring(range.lastIndexOf("=") + 1).trim();
+            String[] ranges = range.split("-");
+            try {
+                //判断range的类型
+                if (ranges.length == 1) {
+                    //类型一：bytes=-2343
+                    if (range.startsWith("-")) {
+                        endByte = Long.parseLong(ranges[0]);
+                    }
+                    //类型二：bytes=2343-
+                    else if (range.endsWith("-")) {
+                        startByte = Long.parseLong(ranges[0]);
+                    }
+                }
+                //类型三：bytes=22-2343
+                else if (ranges.length == 2) {
+                    startByte = Long.parseLong(ranges[0]);
+                    endByte = Long.parseLong(ranges[1]);
+                }
+
+            } catch (NumberFormatException e) {
+                startByte = 0;
+                endByte = file.length() - 1;
+                log.error("Range Occur Error,Message:{}",e.getLocalizedMessage());
+            }
+        }
+
+        //要下载的长度
+        long contentLength = endByte - startByte + 1;
+        //文件名
+        String fileName = file.getName();
+        //文件类型
+        String contentType = request.getServletContext().getMimeType(fileName);
+
+        ////解决下载文件时文件名乱码问题
+        byte[] fileNameBytes = fileName.getBytes(StandardCharsets.UTF_8);
+        fileName = new String(fileNameBytes, 0, fileNameBytes.length, StandardCharsets.ISO_8859_1);
+
+        //各种响应头设置
+        //支持断点续传，获取部分字节内容：
+        response.setHeader("Accept-Ranges", "bytes");
+        //http状态码要为206：表示获取部分内容
+        response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
+        response.setContentType(contentType);
+        response.setHeader("Content-Type", contentType);
+        //inline表示浏览器直接使用，attachment表示下载，fileName表示下载的文件名
+        response.setHeader("Content-Disposition", "inline;filename=" + fileName);
+        response.setHeader("Content-Length", String.valueOf(contentLength));
+        // Content-Range，格式为：[要下载的开始位置]-[结束位置]/[文件总大小]
+        response.setHeader("Content-Range", "bytes " + startByte + "-" + endByte + "/" + file.length());
+
+        BufferedOutputStream outputStream = null;
+        RandomAccessFile randomAccessFile = null;
+        //已传送数据大小
+        long transmitted = 0;
+        try {
+            randomAccessFile = new RandomAccessFile(file, "r");
+            outputStream = new BufferedOutputStream(response.getOutputStream());
+            byte[] buff = new byte[4096];
+            int len = 0;
+            randomAccessFile.seek(startByte);
+            //坑爹地方四：判断是否到了最后不足4096（buff的length）个byte这个逻辑（(transmitted + len) <= contentLength）要放前面！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+            //不然会会先读取randomAccessFile，造成后面读取位置出错，找了一天才发现问题所在
+            while ((transmitted + len) <= contentLength && (len = randomAccessFile.read(buff)) != -1) {
+                outputStream.write(buff, 0, len);
+                transmitted += len;
+            }
+            //处理不足buff.length部分
+            if (transmitted < contentLength) {
+                len = randomAccessFile.read(buff, 0, (int) (contentLength - transmitted));
+                outputStream.write(buff, 0, len);
+                transmitted += len;
+            }
+
+            outputStream.flush();
+            response.flushBuffer();
+            randomAccessFile.close();
+            log.info("下载完毕：" + startByte + "-" + endByte + "：" + transmitted);
+        } catch (ClientAbortException e) {
+            log.warn("用户停止下载：" + startByte + "-" + endByte + "：" + transmitted);
+            //捕获此异常表示拥护停止下载
+        } catch (IOException e) {
+            log.error("用户下载IO异常，Message：{}", e.getLocalizedMessage());
+        } finally {
+            try {
+                if (randomAccessFile != null) {
+                    randomAccessFile.close();
+                }
+            } catch (IOException e) {
+                log.error("关闭文件流失败", e);
+            }
+        }///end try
+    }
+
+}
